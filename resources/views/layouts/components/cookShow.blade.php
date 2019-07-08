dc99398f8a8f4fe908b859578156285f706609b3
seiya endo <seiya.endo0830@gmail.com>
2019-07-08 18:47:39 +0900

    修正

--- resources/views/layouts/components/cookShow.blade.php
+++ resources/views/layouts/components/cookShow.blade.php
@@ -19,12 +19,28 @@
                                         {{ csrf_field() }}
                                         <button type="submit" class="btn btn-sm btn-primary" style="margin-right: 8px;">more</button>
                                     </form>
-                                    <!-- <form action="{{ url('cooks/edit/'.$cook->id) }}" method="GET">
-                                        {{ csrf_field() }} -->
-                                    <button type="submit" class="good btn btn-sm btn-default btn-outline-default btn-icon btn-round btn-{{ $cook->id }} {{ $cook->good }}">
-                                        <i class="far fa-thumbs-up"></i>
-                                    </button>
-                                    <!-- </form> -->
+                                    <div class="goodBtnArea">
+                                        @if (count( $cook->goods->where('user_id', Auth::id()) ) === 0)
+                                            <!-- <form action="{{ url('goods/store') }}" method="POST"> -->
+                                                <!-- {{ csrf_field() }} -->
+                                            <input class="cook_id" type="hidden" name="cook_id" value="{{ $cook->id }}">
+                                            <button type="submit" class="good-store btn btn-sm btn-default btn-outline-default btn-icon btn-round btn-{{ $cook->id }} {{ $cook->good }}">
+                                                <i class="far fa-thumbs-up"></i>
+                                            </button>
+                                            <!-- </form>                                         -->
+                                        @else
+                                            <!-- <form action="{{ url('good/'.$cook->goods->where('user_id', Auth::id())[0]->id) }}" method="POST"> -->
+                                                <!-- {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}  -->
+                                                
+                                                <!-- <input type="hidden" name="cook_id" value="{{ $cook->id }}"> -->
+                                            <input class="good_id" type="hidden" name="good_id" value="{{ $cook->goods->where('user_id', Auth::id())[0]->id }}">
+                                            <button type="submit" class="good-delete btn btn-sm btn-primary btn-outline-primary btn-icon btn-round btn-{{ $cook->id }} {{ $cook->good }}">
+                                                <i class="far fa-thumbs-up"></i>
+                                            </button>
+                                            <!-- </form> -->
+                                        @endif
+                                    </div>
                                 </div>
                                 <small class="text-muted">{{ $cook->created_at }}</small>
                             </div>
