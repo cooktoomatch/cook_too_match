6812f6a8032fd76a7b25fd91a99a7d3ba202bd0b
seiya endo <seiya.endo0830@gmail.com>
2019-07-08 13:26:31 +0900

    ファイルアップロード修正

--- resources/views/cooks_edit.blade.php
+++ resources/views/cooks_edit.blade.php
@@ -12,7 +12,13 @@
                 <input type="text" name="name" value="{{$cook->name}}" class="form-control" placeholder="料理名">
             </div>
             <div class="custom-file">
-                <label for="image" class="control-label" placeholder="サムネイル"></label>
+                <label for="" class="control-label">サムネイル</label>
+                <div id="upArea">
+                    @if ($cook->image)
+                        <img src="{{asset('/storage/cook_image/'.$cook->image)}}" alt="thumbnail" class="upImg img-raised">
+                    @endif
+                </div>
+                <div onclick="$('#image').click();" class="fileBtn form-control">ファイルを選択</div>
                 <input id="image" type="file" class="custom-file-input" name="image" placeholder="サムネイル">
             </div>
             <div class="form-group input-lg">
@@ -42,10 +48,34 @@
 </div>
 @endsection
 
+@section("script")
+<!-- <script src="{{ asset('js/date.js') }}"></script> -->
+<script src="{{ asset('js/fileUp.js') }}"></script>
+@endsection
+
 @section('style')
 <style>
     .submit_btn {
         margin-top: 35px;
+    }
+    .control-label {
+        text-align: left;
+        display: block;
+        box-sizing: border-box;
+        padding-left: 18px;
+        font-size: 13px;
+        margin-top: 26px;
+    }
+    .fileBtn {
+        color: #aaa;
+        cursor: pointer;
+        height: 44px;
+        line-height: 44px;
+        box-sizing: border-box;
+        padding: 0;
+    }
+    .upImg {
+        margin-bottom: 20px;
     }
 </style>
 @endsection
